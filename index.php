a13c5be8e32ebeae9785604b2bf9cf62ddb9323b
Hamza Malikyar <153031850+HamzaMalikyar@users.noreply.github.com>
2024-07-24 22:48:46 -0400

    Update index.php, removing option to proceed without account

--- index.php
+++ index.php
@@ -21,9 +21,6 @@
         <button type="button" class="btn" onclick="location.href='login.php'">Login</button>
         <br>
         <button type="button" class="btn" onclick="location.href='signup.php'" style="margin-top: 10px;">Signup</button>
-        <br>
-                    <button type="button" class="btn" onclick="location.href='home.php'" style="margin-top: 10px;">Proceed Without an Account</button>
-
         <a href="forgot_password.php" style="margin-top: 10px; display: block;">Forgot Password?</a>
         </div>
         </form>
