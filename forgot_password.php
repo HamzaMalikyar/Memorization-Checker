fcb692fbf4141f6037f86f75ce2b445baaa42db1
Hamza Malikyar <153031850+HamzaMalikyar@users.noreply.github.com>
2024-07-27 16:44:53 -0400

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 include("connection.php");
-
-if (!isset($_SESSION['username'])) {
-    header("location: login.php");
-}
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +9,7 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Change Info</title>
+    <title>Forgot Password</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style1.css">
@@ -21,106 +17,107 @@ if (!isset($_SESSION['username'])) {
 
 <body>
 
-    <div class="container">
-        <div class="form-box box">
+<div class="container">
+    <div class="form-box box">
+        <?php
+        if (isset($_POST['verify'])) {
+            $email = $_POST['email'];
+            $query = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'") or die("Error occurs");
 
-            <?php
-            if (isset($_POST['update'])) {
-                $username = $_POST['username'];
-                $email = $_POST['email'];
-                $password = $_POST['password'];
-                $security_question = $_POST['security_question'];
-                $security_answer = $_POST['security_answer'];
+            if (mysqli_num_rows($query) > 0) {
+                $result = mysqli_fetch_assoc($query);
+                $_SESSION['reset_email'] = $result['email'];
+                $_SESSION['reset_security_question'] = $result['security_question'];
+                $_SESSION['reset_security_answer'] = $result['security_answer'];
+            } else {
+                echo "<div class='message'>
+                    <center><p>Email not found. Please try again.</p></center>
+                    </div><br>";
+                exit();
+            }
+        } elseif (isset($_POST['reset'])) {
+            $email = $_SESSION['reset_email'];
+            $security_answer = $_POST['security_answer'];
+            $new_password = $_POST['new_password'];
+            $new_password_hashed = password_hash($new_password, PASSWORD_DEFAULT);
 
-                $id = $_SESSION['id'];
-                $passwd = password_hash($password, PASSWORD_DEFAULT);
-
-                $edit_query = "UPDATE users SET username=?, email=?, password=?, security_question=?, security_answer=? WHERE id=?";
-                $stmt = mysqli_prepare($conn, $edit_query);
-                mysqli_stmt_bind_param($stmt, 'sssssi', $username, $email, $passwd, $security_question, $security_answer, $id);
+            if ($security_answer === $_SESSION['reset_security_answer']) {
+                $update_query = "UPDATE users SET password=? WHERE email=?";
+                $stmt = mysqli_prepare($conn, $update_query);
+                mysqli_stmt_bind_param($stmt, 'ss', $new_password_hashed, $email);
 
                 if (mysqli_stmt_execute($stmt)) {
                     echo "<div class='message'>
-                        <center><p>Profile Updated!</p></center>
+                        <center><p>Password Updated!</p></center>
                         </div><br>";
                     echo "<center><a href='login.php'><button class='btn'>Log in</button></a></center>";
+                    session_unset();
+                    session_destroy();
                 } else {
                     echo "<div class='message'>
                         <center><p>Update Failed. Please try again.</p></center>
                         </div><br>";
                 }
             } else {
-                $id = $_SESSION['id'];
-                $query = mysqli_query($conn, "SELECT * FROM users WHERE id = $id") or die("error occurs");
+                echo "<div class='message'>
+                    <center><p>Incorrect security answer. Please try again.</p></center>
+                    </div><br>";
+            }
+        }
+        ?>
 
-                while ($result = mysqli_fetch_assoc($query)) {
-                    $res_username = $result['username'];
-                    $res_email = $result['email'];
-                    $res_password = $result['password'];
-                    $res_security_question = $result['security_question'];
-                    $res_security_answer = $result['security_answer'];
-                }
-                ?>
+        <?php if (!isset($_POST['verify']) || (isset($_POST['verify']) && isset($result))) { ?>
+            <header>Forgot Password</header>
+            <form action="#" method="POST" enctype="multipart/form-data">
 
-                <header>Change Profile</header>
-                <form action="#" method="POST" enctype="multipart/form-data">
-
+                <?php if (!isset($_SESSION['reset_email'])) { ?>
                     <div class="form-box">
-
-                        <div class="input-container">
-                            <i class="fa fa-user icon"></i>
-                            <input class="input-field" type="text" placeholder="Username" name="username" value="" required>
-                        </div>
-
                         <div class="input-container">
                             <i class="fa fa-envelope icon"></i>
-                            <input class="input-field" type="email" placeholder="Email Address" name="email" value="" required>
+                            <input class="input-field" type="email" placeholder="Email Address" name="email" required>
                         </div>
-
-                        <div class="input-container">
-                            <i class="fa fa-lock icon"></i>
-                            <input class="input-field password" type="password" placeholder="New Password" name="password" value="" required>
-                            <i class="fa fa-eye toggle icon"></i>
+                        <div class="field">
+                            <input type="submit" name="verify" value="Verify Email" class="btn">
                         </div>
-
-                        <!-- Security Question Section -->
+                    </div>
+                <?php } else { ?>
+                    <div class="form-box">
                         <div class="input-container">
                             <i class="fa fa-question icon"></i>
-                            <select class="input-field" name="security_question" required>
-                                <option value="" disabled>Select Security Question</option>
-                                <option value="What is your mother's maiden name?" <?php echo $res_security_question == "What is your mother's maiden name?" ? "selected" : ""; ?>>What is your mother's maiden name?</option>
-                                <option value="What was the name of your first pet?" <?php echo $res_security_question == "What was the name of your first pet?" ? "selected" : ""; ?>>What was the name of your first pet?</option>
-                                <option value="What was the name of your elementary school?" <?php echo $res_security_question == "What was the name of your elementary school?" ? "selected" : ""; ?>>What was the name of your elementary school?</option>
-                            </select>
+                            <p><?php echo $_SESSION['reset_security_question']; ?></p>
                         </div>
                         <div class="input-container">
                             <i class="fa fa-lock icon"></i>
-                            <input class="input-field" type="text" placeholder="Security Answer" name="security_answer" value="" required>
+                            <input class="input-field" type="text" placeholder="Security Answer" name="security_answer" required>
                         </div>
-
+                        <div class="input-container">
+                            <i class="fa fa-lock icon"></i>
+                            <input class="input-field password" type="password" placeholder="New Password" name="new_password" required>
+                            <i class="fa fa-eye toggle icon"></i>
+                        </div>
                     </div>
 
                     <div class="field">
-                        <center><input type="submit" name="update" id="submit" value="Update" class="btn"></center>
+                        <center><input type="submit" name="reset" value="Reset Password" class="btn"></center>
                     </div>
-
-                </form>
-            </div>
+                <?php } ?>
+            </form>
         <?php } ?>
     </div>
+</div>
 
-    <script>
-        const toggle = document.querySelector(".toggle"),
-            input = document.querySelector(".password");
-        toggle.addEventListener("click", () => {
-            if (input.type === "password") {
-                input.type = "text";
-                toggle.classList.replace("fa-eye-slash", "fa-eye");
-            } else {
-                input.type = "password";
-            }
-        })
-    </script>
+<script>
+    const toggle = document.querySelector(".toggle"),
+        input = document.querySelector(".password");
+    toggle.addEventListener("click", () => {
+        if (input.type === "password") {
+            input.type = "text";
+            toggle.classList.replace("fa-eye-slash", "fa-eye");
+        } else {
+            input.type = "password";
+        }
+    })
+</script>
 
 </body>
 
