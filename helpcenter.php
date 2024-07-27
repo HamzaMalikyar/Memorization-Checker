e08251e947fe3a4ffa0162c57bae2ac6c3d08b91
Hamza Malikyar <153031850+HamzaMalikyar@users.noreply.github.com>
2024-07-27 19:22:37 -0400

    Final commit helpcenter.php

--- helpcenter.php
+++ helpcenter.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Help Center - Memorization Checker</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style5.css">
     <style>
         .container {
             width: 80%;
@@ -108,4 +108,3 @@
 </body>
 
 </html>
-
